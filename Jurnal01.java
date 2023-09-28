64804fa1ef5b14589cc8f9911fa6e320adca8f4b
andry adi s <145579144+anxdryy@users.noreply.github.com>
2023-09-28 10:47:10 +0700

    Update Jurnal01.java

--- Jurnal01.java
+++ Jurnal01.java
@@ -1,6 +1,8 @@
 public class Jurnal01{
     public static void main(String[] args) {
-        
+        System.out.println("Nama: Andry Adi Satriya");
+        System.out.println("NIM: 607062300105");
+        System.out.println("Kelas: D3IF4701");
     }
 
 }
